4c9adc8c66b843d43b14b99790f23bfa24f7d5ea
yutatakahassigithub <21c1258@tku.ac.jp>
2023-09-30 10:59:47 +0900

    変更箇所

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+
 export default defineConfig({
+    build: {
+        outDir: 'public/build',
+    },
     plugins: [
         laravel({
             input: [
